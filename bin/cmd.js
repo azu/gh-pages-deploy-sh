4dfba2af952a5da116f9e40d02f41e704ba1b9d6
azu <azuciao@gmail.com>
2015-08-23 22:35:49 +0900

    update

--- bin/cmd.js
+++ bin/cmd.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
-var deploy = require("../lib/gh-pages-deploy");
+var deploy = require("../lib/gh-pages-deploy-sh");
 var globs = process.argv[2];
 deploy(globs).catch(function (error) {
     console.error(error, error.stack);
